a26fed511cdd8f276c35d64975590e38f37f9033
inbum <1234!@>
2022-05-03 15:02:46 +0900

    3.2.1 완료

--- App.js
+++ App.js
@@ -9,34 +9,48 @@
 import React from 'react';
 import type {Node} from 'react';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
+    KeyboardAvoidingView, Platform,
+    ScrollView,
+    StatusBar,
+    StyleSheet,
+    Text,
+    useColorScheme,
+    View,
 } from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import DateHead from './components/DateHead';
+import {SafeAreaProvider} from 'react-native-safe-area-context/src/SafeAreaContext';
+import {SafeAreaView} from 'react-native-safe-area-context';
+import AddTodo from './components/AddTodo';
+import Empty from './components/Empty';
 
 function App() {
+  const today = new Date();
+  console.log(today);
   return (
-      <SafeAreaView>
-        <View>
-          <Text>TodoApp</Text>
-        </View>
-      </SafeAreaView>
+      <SafeAreaProvider>
+          {/*  safeArea를 bottom 하단 부분에만 적용한다는 의미*/}
+          <SafeAreaView edges={['bottom']} style={styles.block}>
+              <KeyboardAvoidingView
+              // behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+                  behavior={Platform.select({ios:'padding', android:undefined})}
+              style={styles.avoid}>
+                  <DateHead date={today} />
+                  <Empty />
+                  <AddTodo />
+              </KeyboardAvoidingView>
+          </SafeAreaView>
+      </SafeAreaProvider>
   )
 }
 
 const styles = StyleSheet.create({
-  
+    block: {
+        flex: 1,
+        backgroundColor: 'rgba(255,255,255,0.99)'
+
+    },
+    avoid: {
+        flex: 1,
+    }
 });
 export default App;
--- components/DateHead.js
+++ components/DateHead.js
@@ -1,22 +1,40 @@
 import React from 'react';
-import {View, StyleSheet, Text} from 'react-native';
+import {View, StyleSheet, Text, StatusBar} from 'react-native';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
 
-function DateHead(props) {
+//useSafeAreaInsets 함수를 사용하여 StautsBar의 높이를 구할 수 있다.
+
+
+function DateHead({date}) {
+    const year = date.getFullYear();
+    const month = date.getMonth()+1 //0 - 11 까지 있으므로 +1
+    const day = date.getDate();
+    const formatted = `${year}년 ${month}월 ${day}일`;
+    const {top} = useSafeAreaInsets();
+
     return (
-        <View style={styles.block}>
-            <Text style={styles.dateText} > 2022년 5월 3일 </Text>
-        </View>
+        <>
+            <View style={[styles.statusBarPlaceholder, {height: top}]} />
+            <StatusBar backgroundColor="#6667AB" barStyle={'light-content'}/>
+            <View style={styles.block}>
+                <Text style={styles.dateText} > {formatted} </Text>
+            </View>
+        </>
     );
 }
 const styles = StyleSheet.create({
     block: {
         padding: 16,
-        backgroundColor: '#26a69a',
+        backgroundColor: '#6667AB',
     },
     dateText: {
-        fontSize: 24,
+        fontSize: 26,
         color: 'white',
+        fontWeight: 'bold'
     },
+    statusBarPlaceholder: {
+        backgroundColor: '#6667AB',
+    }
 });
 
 export default DateHead;
